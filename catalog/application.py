766db854b6f8304d0651fcbe6250c9f5c8fb9da8
Michelle J Levine <michellejl@live.com>
2017-11-27 16:36:48 -0800

    Added templates for categories, items, and new category pages

--- catalog/application.py
+++ catalog/application.py
@@ -1,42 +1,84 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
+
+# Fake Data (TEMP)
+category = {'name': 'Dev Tools', 'id': '1'}
+categories = [
+  {'name': 'Dev Tools', 'id': '1'},
+  {'name': 'IDEs', 'id': '2'},
+  {'name': 'Documents', 'id': '3'},
+]
+items = [
+  {
+    'name': 'Transmit',
+    'description': 'this is an app',
+    'image_url': 'transmit.png',
+    'website': 'https://panic.com/transmit/',
+    'id': '1'
+  },
+  {
+    'name': 'PyCharm',
+    'description': 'this is also an app',
+    'image_url': 'pycharm.png',
+    'website': 'https://www.jetbrains.com/pycharm/',
+    'id': '2'
+  },
+  {
+    'name': 'webstorm',
+    'description': 'yet another app. yay',
+    'image_url': 'webstorm.png',
+    'website': 'https://www.jetbrains.com/webstorm/',
+    'id': '3'
+  }
+]
+
 
 @app.route('/')
 @app.route('/categories')
 def showCategories():
-  return "Category list"
+  return render_template(
+    'categories.html', categories=categories
+  )
 
 @app.route('/category/<int:category_id>')
 @app.route('/category/<int:category_id>/items')
-def showItemsList():
-  return "Items list"
+def showItemsList(category_id):
+  return render_template(
+    'itemlist.html', category=category, items=items
+  )
 
-@app.route('/category/new')
+@app.route('/category/new', methods=['GET', 'POST'])
 def newCategory():
-  return "New Category"
+  if request.method == 'POST':
+    newCategory = Category(name=request.form['name'])
+    session.add(newCategory)
+    session.commit()
+    return redirect(url_for('showCategories'))
+  else:
+    return render_template('newcategory.html')
 
 @app.route('/category/<int:category_id>/edit')
-def editCategory():
+def editCategory(category_id):
   return "Edit Category"
 
 @app.route('/category/<int:category_id>/delete')
-def deleteCategory():
+def deleteCategory(category_id):
   return "Delete Category"
 
 @app.route('/category/<int:category_id>/item/<int:item_id>')
-def itemDetails():
+def itemDetails(category_id, item_id):
   return "Item Details"
 
 @app.route('/category/<int:category_id>/item/new')
-def newItem():
+def newItem(category_id):
   return "New Item"
 
 @app.route('/category/<int:category_id>/item/<int:item_id>/edit')
-def editItem():
+def editItem(category_id, item_id):
   return "Edit Item"
 
 @app.route('/category/<int:category_id>/item/<int:item_id>/delete')
-def deleteItem():
+def deleteItem(category_id, item_id):
   return "Delete Item"
 
 
